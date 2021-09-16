a37c7477416459740ace11a19825500bfb306d9d
iborymagic <mlch8023@gmail.com>
2021-09-17 04:54:53 +0900

    fix: removeEventListener 수정 - 반응형이 실시간으로 동작하지 않던 문제 수정

--- src/index.js
+++ src/index.js
@@ -6,25 +6,31 @@ const mobileNavMenu = document.querySelector('.mobile-nav-menu');
 const footer = document.querySelector('footer');
 const footerCheckboxes = footer.querySelectorAll('input[type="checkbox"]');
 
-if (mobile.matches) {
-  navMenu.classList.add('hidden');
-  mobileNavMenu.classList.remove('hidden');
-  footerCheckboxes.forEach((checkbox) => {
-    checkbox.classList.remove('hidden');
-    checkbox.addEventListener('change', (e) => {
-      const menuList = e.target.closest('.menu').querySelector('ul');
-      if (checkbox.checked) {
-        menuList.classList.add('menu-visible');
-      } else {
-        menuList.classList.remove('menu-visible');
+const setResponsiveUI = () => {
+  if (mobile.matches) {
+    navMenu.classList.add('hidden');
+    mobileNavMenu.classList.remove('hidden');
+    footerCheckboxes.forEach((checkbox) => {
+      checkbox.classList.remove('hidden');
+      checkbox.addEventListener('change', function setResponsiveCheckbox(e) {
+        const menuList = e.target.closest('.menu').querySelector('ul');
+        if (checkbox.checked) {
+          menuList.classList.add('menu-visible');
+        } else {
+          menuList.classList.remove('menu-visible');
+        }
+      });
+    });
+  } else {
+    navMenu.classList.remove('hidden');
+    mobileNavMenu.classList.add('hidden');
+    footerCheckboxes.forEach((checkbox) => {
+      checkbox.classList.add('hidden');
+      if (typeof setResponsiveCheckbox === 'function') {
+        checkbox.removeEventListener('change', setResponsiveCheckbox);
       }
     });
-  });
-} else {
-  navMenu.classList.remove('hidden');
-  mobileNavMenu.classList.add('hidden');
-  footerCheckboxes.forEach((checkbox) => {
-    checkbox.classList.add('hidden');
-    checkbox.removeEventListener('change');
-  });
-}
+  }
+};
+
+window.addEventListener('resize', setResponsiveUI);
